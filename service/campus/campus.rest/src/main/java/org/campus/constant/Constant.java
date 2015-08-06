21bc2d5abd287c8419ddd57407c7671a1f872a64
嵇磊 <jilei@lianchuang.com>
2015-08-06 11:10:55 +0800

    修改文件编码

--- service/campus/campus.rest/src/main/java/org/campus/constant/Constant.java
+++ service/campus/campus.rest/src/main/java/org/campus/constant/Constant.java
@@ -4,6 +4,6 @@ public interface Constant {
 
     String CAMPUS_SECURITY_SESSION = "CAMPUS_SECURITY_SESSION";
 
-    int CACHE_TIME = 1800;// Ĭ�ϻ���ʱ��,��λ��
+    int CACHE_TIME = 1800;// 默认缓存时间,单位秒
 
 }
